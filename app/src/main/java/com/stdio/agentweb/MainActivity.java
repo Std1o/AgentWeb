d910d3f3800000cffc86ae311dcb01966f494755
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-02 16:52:57 +0300

    0.0.1

--- app/src/main/java/com/stdio/agentweb/MainActivity.java
+++ app/src/main/java/com/stdio/agentweb/MainActivity.java
@@ -1,14 +1,70 @@
 package com.stdio.agentweb;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.AgentWebConfig;
+import com.just.agentweb.WebChromeClient;
+import com.just.agentweb.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String WEB_URL = "https://vk.com";
+    AgentWeb agentWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = findViewById(R.id.webLayout);
+
+        agentWeb = AgentWeb.with(this)
+                .setAgentWebParent(linearLayout, new LinearLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator()
+                .setWebChromeClient(new WebChromeClient())
+                .setWebViewClient(mWebViewClient)
+                .createAgentWeb()
+                .ready()
+                .go(WEB_URL);
+    }
+
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            AgentWebConfig.syncCookie(url, AgentWebConfig.getCookiesByUrl(url));
+            super.onPageFinished(view, url);
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (agentWeb.handleKeyEvent(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onPause() {
+        agentWeb.getWebLifeCycle().onPause();
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        agentWeb.getWebLifeCycle().onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        agentWeb.getWebLifeCycle().onDestroy();
+        super.onDestroy();
     }
 }
